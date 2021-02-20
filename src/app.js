76e4cc91cc76ed96bacdae49398105ea0b6b1861
tom <tomas.b.lp@gmail.com>
2021-02-20 03:49:21 -0300

    textarea clipboard

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import 'babel-polyfill' // polyfill async/await
 import './style.css'
@@ -54,7 +54,7 @@ const App = props => {
             [``]
         ]
 
-        setCommitsCommand(commands.join('\n'))
+        return commands.join('\n')
     }
 
     return <div id='layout'>
@@ -64,16 +64,39 @@ const App = props => {
             click - draw <br/>
             click + shift - delete
             </div>
-            <div id='generator'>
-                <svg class="octicon octicon-repo UnderlineNav-octicon hide-sm" height="16" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"></path></svg>
-                REMOTE: <input defaultValue={origin} onChange={e=>setOrigin(e.target.value)}/>
-                <button onClick={generateCommits}>win</button>
-                <button onClick={generateCommits}>linux</button>
-                <textarea readOnly value={commitsCommand}></textarea>
-            </div>
+            <OriginInput origin={origin} setOrigin={setOrigin}/>
+            <TextareaCopy commands={origin ? generateCommits() : ''} />
     </div>
 
 }
+
+const TextareaCopy = props => {
+
+    const textarea = useRef(null)
+
+    const clipboard = ()=>{
+        // copy props.commands
+        textarea.current.select()
+        document.execCommand('copy')
+    }
+
+    return <>
+        <button id='copy' onClick={clipboard}>copy to clipboard!</button>
+        <textarea readOnly ref={textarea} value={props.commands}></textarea>
+    </>
+}
+
+const OriginInput = props => {
+    let {origin, setOrigin} = props
+    return <div id='origin'>
+    <svg className="octicon octicon-repo UnderlineNav-octicon hide-sm" height="16" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true">
+        <path fillRule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"></path>
+    </svg>
+    remote repository:
+    <input placeholder='https://github.com/<user>/<repo>' value={origin} onChange={e=>setOrigin(e.target.value)}/>
+    </div>
+}
+
 
 const SearchBox = props => {
 
